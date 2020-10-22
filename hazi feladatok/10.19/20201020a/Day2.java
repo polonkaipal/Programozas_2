dae197a8f3c4ef52c5b242c8fbfec4ea3a6cd901
Polonkai Pál <polonkaipal5@gmail.com>
2020-10-22 10:26:43 +0200

    Második fele is kész

--- hazi feladatok/10.19/20201020a/Day2.java
+++ hazi feladatok/10.19/20201020a/Day2.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Day2 {
@@ -7,18 +9,22 @@ public class Day2 {
         int szum = 0;
         for (int i = 0; i < tablazat.size(); i++) {
             String[] reszek = tablazat.get(i).split("\t");
-            int min = Integer.parseInt(reszek[0]);
-            int max = Integer.parseInt(reszek[0]);
-            for (int j = 1; j < reszek.length; j++) {
-                int tmp = Integer.parseInt(reszek[j]);
-                if (tmp < min) {
-                    min = tmp;
-                }
-                if (tmp > max) {
-                    max = tmp;
+            List<Integer> szamok = new ArrayList<Integer>();
+            for (int j = 0; j < reszek.length; j++) {
+                szamok.add(Integer.parseInt(reszek[j]));
+            }
+            Collections.sort(szamok);
+            Collections.reverse(szamok);
+            for (int j = 0; j < szamok.size() - 1; j++) {
+                for (int k = j + 1; k < szamok.size(); k++) {
+                    int tmp1 = szamok.get(j);
+                    int tmp2 = szamok.get(k);
+                    if (tmp1 % tmp2 == 0) {
+                        szum += tmp1 / tmp2;
+                        continue;
+                    }
                 }
             }
-            szum += max - min;
         }
         System.out.println("A cheksum összege: " + szum);
     }
